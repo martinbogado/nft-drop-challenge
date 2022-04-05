c1b31ee187eb23252c73a7d71d56d251161126d6
martinbogado <martinbogado@live.com.ar>
2022-04-04 22:58:54 -0300

    set typings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+import { sanityClient, urlFor } from '../sanity'
+import { Collection } from '../typings'
+
+interface Props {
+  collections: Collection[]
+}
+
+const Home = ({ collections }: Props) => {
   return (
     <div className="">
       <Head>
@@ -17,3 +24,40 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `*[_type == "collection"]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage{
+    asset
+    },
+    previewImage{
+      asset
+     },
+  slug{
+    current
+  },
+  creator->{
+    _id,
+    name,
+    address,
+    slug{
+    current
+  },
+  },
+    }`
+
+  const collections = await sanityClient.fetch(query)
+
+  console.log(collections)
+
+  return {
+    props: {
+      collections,
+    }
+  }
+}
