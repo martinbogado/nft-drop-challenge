8d5686f7df25ac0558cc5bea003a402f9468f050
martinbogado <martinbogado@live.com.ar>
2022-04-10 01:45:16 -0300

    sm screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home = ({ collections }: Props) => {
       <ChangeTheme />
 
       {/* Header */}
-      <h1 className='absolute text-3xl lg:text-4xl lg:top-10 font-extralight text-center'>
+      <h1 className='text-3xl lg:text-4xl font-extralight text-center lg:text-left lg:absolute lg:top-10'>
         The{' '}
         <span className='font-extrabold underline decoration-yellow-600/50'>
             PAPAFAM 
@@ -34,22 +34,22 @@ const Home = ({ collections }: Props) => {
       </h1>
 
       {/* Landing */}
-      <div className='grid grid-rows-2 justify-between h-screen lg:grid-cols-2 md:grid-cols-2 box-border'>
-        <div className="col-span-2 md:col-span-1 lg:col-span-1 lg:row-span-2 mb-12 mt-24 sm:mt-16 flex flex-col space-y-6 text-center md:mb-0 md:text-left md:justify-center md:space-y-1 lg:justify-center lg:space-y-2">
+      <div className='grid grid-rows-2 justify-between h-[90vh] lg:h-screen lg:grid-cols-2 md:grid-cols-2 box-border'>
+        <div className="col-span-2 md:col-span-1 lg:row-span-2 mb-12 mt-8 sm:mt-16 flex flex-col space-y-6 text-center md:mb-0 md:text-left md:justify-center md:space-y-2">
           
           <h1 className=" text-3xl font-extralight dark:text-white  md:max-w-md md:text-6xl lg:font-bold">
             Discover and collect rare <span className="font-bold text-yellow-600">NFTs</span>
           </h1>
         </div>
           <div className='md:col-span-1 md:row-span-2 md:relative'>
-            <div className='absolute md:top-1/2 lg:top-1/2 w-max lg:hover:-translate-y-[220px] transition duration-300 -translate-y-[180px] translate-x-[200px] sm:-translate-y-[180px] sm:translate-x-[220px] lg:-translate-y-[200px] lg:translate-x-[300px]'>
+            <div className='absolute md:top-1/2 lg:top-1/2 w-max lg:hover:-translate-y-[220px] transition duration-300 -translate-y-[180px] translate-x-[200px] sm:-translate-y-[180px] sm:translate-x-[350px] md:translate-x-[220px] lg:-translate-y-[200px] lg:translate-x-[300px]'>
               <div className=' w-32 sm:w-48 card transition rounded-sm lg:w-72 '>
                 
                   <img src="/images/punk.png" alt="punk" />
                 
               </div> 
             </div>
-            <div className='absolute md:top-1/2 lg:top-1/2 w-max lg:hover:-translate-y-[120px] transition duration-300 -translate-y-[80px] translate-x-[100px] sm:-translate-y-[80px] sm:translate-x-[120px] lg:-translate-y-[100px] lg:translate-x-[160px]'>
+            <div className='absolute md:top-1/2 lg:top-1/2 w-max lg:hover:-translate-y-[120px] transition duration-300 -translate-y-[80px] translate-x-[100px] sm:-translate-y-[80px] sm:translate-x-[180px] md:translate-x-[120px] lg:-translate-y-[100px] lg:translate-x-[160px]'>
               <div className=' w-32 sm:w-48 card transition rounded-sm lg:w-72'>
                 
                   <img src="/images/something.png" alt="something" />
