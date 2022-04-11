93401791e2f0b1436f4a86795d4e4aaaba0f7edd
martinbogado <martinbogado@live.com.ar>
2022-04-11 00:05:41 -0300

    important info tooltip

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -152,13 +152,21 @@ function NFTDropPage({ collection }: Props ) {
             {/* Header */}
             <header className='flex items-center justify-between lg:mr-16'>
                 <Link href={'/'}>
-                    <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>
+
+                    <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80 lg:flex lg:space-x-2'>
                         The{' '}
-                        <span className='font-extrabold underline decoration-pink-600/50'>
+                        <span className='font-extrabold underline decoration-pink-600/50 lg:ml-1 lg:mr-1'>
                             PAPAFAM 
                         </span>{' '}
                         NFT Market Place
+                        <span className="group hidden lg:block mt-1 cursor-auto">             
+                                <img className='dark:invert' src="/static/images/information.png" alt="info" /> 
+                                <span className=" bg-gray-400 dark:bg-gray-300 text-black font-bold text-base p-3 mt-2 -ml-[14%] rounded max-w-[30vw] hidden group-hover:block absolute text-center py-2 px-6 z-50">
+                                    These are NOT the original tokens, they are copies from the original collection. The original NFT retains all its value, while this copy has none. They're not meant to generate any profit, only for learning purposes.
+                                </span>      
+                        </span>
                     </h1>
+
                 </Link>
                 
                 <button onClick={() => address ? disconnect() : connectWithMetamask()}
